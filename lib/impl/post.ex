3b9c6539f5bf876309118f3c888d9177f06a821a
Wendell Richards <wenrich85@gmail.com>
2021-12-19 10:31:40 -0500

    updated post parser

--- lib/impl/post.ex
+++ lib/impl/post.ex
@@ -4,9 +4,9 @@ defmodule LearnedEffort.Impl.Post do
 
 
   defstruct(
-    title: String,
+    title: nil,
     description: String,
-    body: String,
+    body: nil,
     cta: String,
     status: :draft,
     views: 0,
@@ -14,10 +14,19 @@ defmodule LearnedEffort.Impl.Post do
     image: nil
   )
 
-  def new_post(post) do
+  @opaque t :: %__MODULE__{title: String.t()}
+
+  @spec new_post(map()) :: {:ok, t() |:error, atom()}
+
+
+  def new_post(%{title: _title, body: body}=post) when is_binary(body) do
     struct(__MODULE__, post)
       |> parse_body_markdown
   end
+
+  def new_post(%{title: _title}=post), do: struct(__MODULE__, post)
+
+  def new_post(_post), do: {:error, "title is required"}
 
   def increment_views(post) do
     %{ post | views: post.views + 1 }
@@ -29,6 +38,7 @@ defmodule LearnedEffort.Impl.Post do
 
   defp parse_body_markdown(post) do
     %{post | body: Earmark.as_html! post.body}
+
   end
 
 end
